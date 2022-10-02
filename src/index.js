85d3271e2ab031974172db9c8deab25df09d6fea
pradeeprajr93 <pradeeprajr93@gmail.com>
2022-10-02 09:11:29 +0000

    fix: moved app js file to bin folder

--- src/index.js
+++ src/index.js
@@ -4,40 +4,46 @@ const config = require('./config/config');
 const logger = require('./config/logger');
 var configData = require("./config/connection");
 
-configData.getConnectionInfo().then(
-  (dbConnection) => {
-    let server;
-    mongoose.connect(dbConnection.DATABASE_URL, config.mongoose.options).then(() => {
-      logger.info('Connected to MongoDB');
-      server = app.listen(config.port, () => {
-        logger.info(`Listening to port ${config.port}`);
-      });
-    });
-
-    const exitHandler = () => {
-      if (server) {
-        server.close(() => {
-          logger.info('Server closed');
-          process.exit(1);
+async function getApp() {
+  configData.getConnectionInfo().then(
+    (dbConnection) => {
+      let server;
+      mongoose.connect(dbConnection.DATABASE_URL, config.mongoose.options).then(() => {
+        logger.info('Connected to MongoDB');
+        server = app.listen(config.port, () => {
+          logger.info(`Listening to port ${config.port}`);
         });
-      } else {
-        process.exit(1);
-      }
-    };
+      });
+  
+      const exitHandler = () => {
+        if (server) {
+          server.close(() => {
+            logger.info('Server closed');
+            process.exit(1);
+          });
+        } else {
+          process.exit(1);
+        }
+      };
+  
+      const unexpectedErrorHandler = (error) => {
+        logger.error(error);
+        exitHandler();
+      };
+  
+      process.on('uncaughtException', unexpectedErrorHandler);
+      process.on('unhandledRejection', unexpectedErrorHandler);
+  
+      process.on('SIGTERM', () => {
+        logger.info('SIGTERM received');
+        if (server) {
+          server.close();
+        }
+      });
+    }
+  );
+}
 
-    const unexpectedErrorHandler = (error) => {
-      logger.error(error);
-      exitHandler();
-    };
-
-    process.on('uncaughtException', unexpectedErrorHandler);
-    process.on('unhandledRejection', unexpectedErrorHandler);
-
-    process.on('SIGTERM', () => {
-      logger.info('SIGTERM received');
-      if (server) {
-        server.close();
-      }
-    });
-  }
-);
+module.exports = {
+  getApp
+};
